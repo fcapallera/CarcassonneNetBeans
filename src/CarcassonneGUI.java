6205bf5e7d792ae0e7900d6460174b318ebab8d3
GuzZ-EniX <adriaorellana@hotmail.com>
2019-04-03 16:29:26 +0200

    Moved game logic from Joc to GUI and deleted Joc class

--- src/CarcassonneGUI.java
+++ src/CarcassonneGUI.java
@@ -4,48 +4,40 @@
  * and open the template in the editor.
  */
 
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
-import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
 import javafx.scene.image.Image ;
-import static java.lang.Double.SIZE;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Stack;
 import javafx.application.Application;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.geometry.VPos;
-import javafx.scene.AccessibleRole;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
 import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.DataFormat;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
-import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.scene.transform.Rotate;
-import javafx.scene.transform.Transform;
 import javafx.stage.Stage;
-import static jdk.nashorn.internal.objects.NativeRegExp.source;
 
 
 /**
@@ -57,6 +49,67 @@ public class CarcassonneGUI extends Application {
     private int col;
     private int row;
     
+    static final String PROVES_SRC = "src/proves/";
+
+    private Stack<Peça> _peces;
+    private ArrayList<Jugador> _jugadors;
+    private Tauler _tauler;
+
+    public void init(String arxiu){
+        File f = new File(PROVES_SRC+arxiu+".txt");
+        HashMap<String,Integer> peces = new HashMap<>();
+        boolean error = false;
+        try {
+            Scanner lector = new Scanner(f);
+
+            //Llegim els jugadors
+            if(lector.hasNext() && lector.next().equals("nombre_jugadors")){
+                int njugs = lector.nextInt();
+                for(int i=0;i<njugs;i++) _jugadors.add(new Jugador(i));
+                if(lector.hasNext() && lector.next().equals("jugadors_cpu")){
+                    while(lector.hasNextInt()){
+                        int j_cpu = lector.nextInt();
+                        _jugadors.get(j_cpu-1).setCpu();
+                    }
+                } else error = true;
+            } else error = true;
+
+
+            //Llegim les peces
+            if(lector.hasNext() && lector.next().equals("rajoles")){
+                String codi;
+                while(!(codi = lector.next()).equals("#")) {
+                    Integer nombre = lector.nextInt();
+                    peces.put(codi, nombre);
+                }
+            } else error = true;
+
+            //Llegim la rajola inicial
+            if(lector.hasNext() && lector.next().equals("rajola_inicial")){
+                String inicial = lector.next();
+                peces.put(inicial,peces.get(inicial)-1);
+                _tauler.afegirPeça(new Peça(inicial),0,0);
+            } else error = true;
+
+        } catch (FileNotFoundException e){
+            System.err.println(e);
+        }
+
+        //Afegim les peces al stack
+        if(!error){
+            Iterator it = peces.entrySet().iterator();
+            while(it.hasNext()){
+                Map.Entry pair = (Map.Entry)it.next();
+                for(int i=0;i<(Integer)pair.getValue();i++){
+                    Peça p = new Peça((String)pair.getKey());
+
+                    _peces.push(p);
+                }
+            }
+            Collections.shuffle(_peces);
+        } else System.out.println("Error");
+    }
+    
     
     
     private BorderPane root;
@@ -64,11 +117,14 @@ public class CarcassonneGUI extends Application {
     private GridPane dreta;
     private Button rota;
     private ImageView pila;
-    private Joc j;
     private int rotacioPila; 
     
     @Override
     public void start(Stage primaryStage) {
+        _peces = new Stack<>();
+        _jugadors = new ArrayList<>();
+        _tauler = new Tauler(false);
+        init("1");
         row = 9;
         col = 8;
         numAux = 690;
@@ -355,7 +411,6 @@ public class CarcassonneGUI extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Joc _joc = new Joc("1");
         launch(args);
     }
     
